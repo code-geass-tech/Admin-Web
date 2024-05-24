a8da11bc45a0a3cae8b0aeb471572108f78decd1
2wons <013ayu@gmail.com>
2024-05-25 02:54:04 +0800

    add open sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif']
+    },
     container: {
       center: true,
       padding: "2rem",
